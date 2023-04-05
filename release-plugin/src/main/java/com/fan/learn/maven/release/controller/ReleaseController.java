1aa9564c317a328529c9ea7a47201ca3f6e09ecb
jianbo <jianbo.fan2016@gmail.com>
2023-04-05 21:55:59 +0800

    add flux demo

--- release-plugin/src/main/java/com/fan/learn/maven/release/controller/ReleaseController.java
+++ release-plugin/src/main/java/com/fan/learn/maven/release/controller/ReleaseController.java
@@ -3,9 +3,12 @@ package com.fan.learn.maven.release.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.Optional;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Author jianbo
@@ -20,5 +23,19 @@ public class ReleaseController {
     @GetMapping("/mvn/plugin/release")
     public Mono<String> helloRelease(@RequestParam(required = false) String name) {
         return Mono.defer(() -> Mono.just("hello," + Optional.ofNullable(name).orElse("jianbo")));
+    }
+
+    @GetMapping("/flux")
+    public Flux<String> flux() {
+        return Flux.create(sink -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                }
+                sink.next(ThreadLocalRandom.current().nextInt(1000, 9999) + "\r\n");
+            }
+            sink.complete();
+        });
     }
 }
