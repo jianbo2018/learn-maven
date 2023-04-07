563d6cdc68b62ff36f49105e8eb723cd7ffbd3ea
jianbo <jianbo.fan2016@gmail.com>
2023-04-07 16:15:47 +0800

    测试多模块环境下，release plugin的表现

---
+++ hello-maven/src/main/java/com/fan/learn/maven/hello/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.fan.learn.maven.hello;
+
+/**
+ * @Author jianbo
+ * @Date 2023/4/7 16:15
+ * @Version 1.0
+ * @Description <br/>
+ *
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
